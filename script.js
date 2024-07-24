ebcec5aaf8ced2cf06e4d65f57730142f9631422
Yan Carlos <yansantos2332@gmail.com>
2024-07-24 14:23:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function armazenarValor(){
 var userInput = document.getElementById("userInput").value;
 
 // exibe o valor armazenado
-console.log("o valor da variavel é: " + userInput);
+console.log("o valor da variavel é:" + userInput);
 
 // atualiza o conteudo 
-document.getElementById("valorInserido").innerText = "O valor inserido é" + userInput;
+document.getElementById("valorInserido").innerText = "O valor inserido é " + userInput;
 
 }
